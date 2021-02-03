5c8fc846feb9bbc35b3689fad573791df16a151b
lotfizs <2133349@talnet.nl>
2021-02-03 11:43:02 +0100

    update

---
+++ php/h02/op2.2/index.php
@@ -0,0 +1,12 @@
+<?php
+$a = "ik heb niet gefaald ";
+
+$b = "die niet werken ";
+
+$c = 1000;
+
+$d = "ik heb alleen ";
+
+$e = " manieren gevonden ";
+
+echo $a.$d.$c.$e.$b."<br>"."-Thomas Edison";
