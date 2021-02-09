7fc43721048617cf0497b1a042049e5a7a82c9cf
lotfizs <2133349@talnet.nl>
2021-02-09 14:05:54 +0100

    update

---
+++ php/h05/dynamische website/resultaat.php
@@ -0,0 +1,17 @@
+<?php
+if ($_POST["inlognaam"] == "") {
+    echo "Vul je naam in!!";
+    echo "<br><br> <a href='formulier.html'> Terug naar het formulier</a>";
+}
+if ($_POST["adress"] == "") {
+    echo "Vul je adress in!!";
+    echo "<br><br> <a href='formulier.html'> Terug naar het formulier</a>";
+}
+if ($_POST["mail"] == "") {
+    echo "Vul je Email in!!";
+    echo "<br><br> <a href='formulier.html'> Terug naar het formulier</a>";
+}
+if ($_POST["wachtwoord"] == "") {
+    echo "Vul je wachtwoord in!!";
+    echo "<br><br> <a href='formulier.html'> Terug naar het formulier</a>";
+}
