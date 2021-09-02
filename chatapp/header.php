2febf6ac7296682960ce78df48c24a3ac05ec59e
lotfizs <2133349@talnet.nl>
2021-09-02 15:46:49 +0200

    tijdlijn

---
+++ chatapp/header.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en" xmlns="http://www.w3.org/1999/html">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>lzs chatapp</title>
+    <nav class="navbar">
+        <div class="max-width">
+            <div class="logo"><a href="#">Lotfi<span>ZS.</span></a></div>
+    </nav>
+
+
+  <link rel="stylesheet" href="style.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css"/>
+
+</head>
+
